d7df9216debd0f0cf858cc0d6d67b2ee8dd1d8ba
enesbedir <enesbedir92@gmail.com>
2025-03-22 14:38:09 +0300

    Store güncellendi - Yeni reducerlar eklendi

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -4,6 +4,10 @@ import serverReducer from '../features/servers/serverSlice';
 import roomReducer from '../features/rooms/roomSlice';
 import socketReducer from '../features/socket/socketSlice';
 import settingsReducer from '../features/settings/settingsSlice';
+import friendReducer from '../features/friends/friendSlice';
+import messageReducer from '../features/messages/messageSlice';
+import channelReducer from '../features/channels/channelSlice';
+import roleReducer from '../features/roles/roleSlice';
 
 export const store = configureStore({
   reducer: {
@@ -12,5 +16,9 @@ export const store = configureStore({
     rooms: roomReducer,
     socket: socketReducer,
     settings: settingsReducer,
+    friends: friendReducer,
+    messages: messageReducer,
+    channels: channelReducer,
+    roles: roleReducer,
   },
 });
